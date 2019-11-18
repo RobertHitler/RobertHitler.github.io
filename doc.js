4bff0e7687e9061253e7babee50b100603a15efb
RobertHitler <robert.h.parikrma@gmail.com>
2019-11-18 17:06:36 +0530

    update

--- doc.js
+++ doc.js
@@ -1,3 +1,6 @@
+
+/*---email__copy_function---*/
+
 function myFunction() {
 		var copyText = document.getElementById("myInput");
 		copyText.select();
@@ -5,7 +8,9 @@ function myFunction() {
 		alert("Copied the text: " + copyText.value);
         console.log("Hello world")
 }
-    
+   
+
+/*---picture change_function----*/
     
 document.querySelector('.my_pic').onmouseover = function(){hover1()};
   function hover1(){
@@ -21,6 +26,9 @@ document.querySelector('.my_pic').onmouseout = function(){hover2()};
       
   }
     
+
+
+/*===mobile _bar menu function===*/
     
     function mob_menu(){
         var icon = document.querySelector('.navi');
@@ -34,3 +42,46 @@ document.querySelector('.my_pic').onmouseout = function(){hover2()};
         }
     }
    
+
+/*=====works_content function====*/
+
+
+
+
+function web(){
+        document.getElementById('cont_1').href = '#web_sec';
+        x1.display = 'block';
+        x2.display = 'none';
+        x3.display = 'none';
+        x4.display = 'none';
+        x5.display = 'none';
+        
+    }
+
+
+function prod(){
+    document.getElementById('cont_2').href = '#prod_sec';
+    x1.display = 'none';
+    x2.display = 'block';
+    x3.display = 'none';
+    x4.display = 'none';
+    x5.display = 'none';
+}
+
+function model(){
+    document.getElementById('cont_3').href = '#model_sec_1';
+    x1.display = 'none';
+    x2.display = 'none';
+    x3.display = 'block';
+    x4.display = 'block';
+    x5.display = 'none';
+}
+
+function aug_re(){
+    document.getElementById('cont_4').href = '#ar_sec';
+    x1.display = 'none';
+    x2.display = 'none';
+    x3.display = 'none';
+    x4.display = 'none';
+    x5.display = 'block';
+}
